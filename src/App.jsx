5272ace97574f96251702da440884219460b2813
Jefer Setiawan <jefersetiawan04@gmail.com>
2022-06-29 12:50:07 +0700

    data history implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import { Flex } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import Header from './components/header';
 import Body from './components/body';
-import History from './components/history';
 
 
 function App() {
   return (
     <Flex className="App" h='100vh' textAlign='center' flexDir='column' alignItems='center' justifyContent='center' textColor='#EEEEEE'>
       <Header />
-      <Flex gap='60px' my='50px' flexDir={{ base: 'column', lg: 'row' }} >
-        <Body />
-        <History />
-      </Flex>
+      <Body />
     </Flex>
   );
 }
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,8 +1,8 @@
 import { React, useState } from "react";
-import { Box, Button, FormControl, NumberInput, NumberInputField, FormHelperText, FormErrorMessage, Heading } from "@chakra-ui/react";
+import { Box, Button, FormControl, NumberInput, NumberInputField, FormHelperText, FormErrorMessage, Heading, Flex } from "@chakra-ui/react";
+import History from "./history";
 
-
-export default function Body(props) {
+export default function Body() {
     const [saldo, setSaldo] = useState(0);
     const [input, setInput] = useState(0);
     const [data, setData] = useState({
@@ -18,7 +18,17 @@ export default function Body(props) {
     }
 
     function handleSubmit() {
-        // e.preventDefault();
+        if (parseInt(input) > 0) {
+            setData({
+                Expense: [...data.Expense],
+                Income: [...data.Income, parseInt(input)],
+            });
+        } else {
+            setData({
+                Expense: [...data.Expense, parseInt(input)],
+                Income: [...data.Income],
+            });
+        }
         setSaldo((prevVal) => prevVal + parseInt(input));
         console.log("masuk");
     }
@@ -33,24 +43,27 @@ export default function Body(props) {
     }
 
     return (
-        <Box className="Body">
-            <Box className='Saldo'>
-                <Heading fontSize='lg' my='10px'>Your Balance: Rp{saldo}</Heading>
+        <Flex className="Bungkus" gap='20px' flexDir='column' justifyContent='center' alignItems='center'>
+            <Box className="Body">
+                <Box className='Saldo'>
+                    <Heading fontSize='lg' my='10px'>Your Balance: Rp{saldo}</Heading>
+                </Box>
+                <Box className='InputUser' as='form' width='200px' onSubmit={check}>
+                    <FormControl isInvalid={isError} my='10px'>
+                        <NumberInput max={100000000} min={-100000000}>
+                            <NumberInputField onChange={updateInput} borderColor='#393E46' bg='#00ADB5' color='#0A0908' _hover={{ color: '#0A0908', bg: '#00ADB5' }} />
+                        </NumberInput>
+                        {!isError ? (
+                            <FormHelperText>
+                            </FormHelperText>
+                        ) : (
+                            <FormErrorMessage>Input Can't Be Empty.</FormErrorMessage>
+                        )}
+                        <Button my='7px' type="submit" color='#0A0908' bg='#00ADB5' _hover={{ color: '#0A0908', bg: '#00ADB5' }}> Hitung </Button>
+                    </FormControl>
+                </Box>
             </Box>
-            <Box className='InputUser' as='form' onSubmit={check} display='flex' justifyContent='center' alignItems='center' flexDir='column'>
-                <FormControl isInvalid={isError} my='10px'>
-                    <NumberInput max={100000000} min={-100000000}>
-                        <NumberInputField onChange={updateInput} borderColor='#393E46' bg='#00ADB5' color='#0A0908' _hover={{ color: '#0A0908', bg: '#00ADB5' }} />
-                    </NumberInput>
-                    {!isError ? (
-                        <FormHelperText>
-                        </FormHelperText>
-                    ) : (
-                        <FormErrorMessage>Input Can't Be Empty.</FormErrorMessage>
-                    )}
-                    <Button my='7px' type="submit" color='#0A0908' bg='#00ADB5' _hover={{ color: '#0A0908', bg: '#00ADB5' }}> Hitung </Button>
-                </FormControl>
-            </Box>
-        </Box>
+            <History data={data} />
+        </Flex>
     )
 }
