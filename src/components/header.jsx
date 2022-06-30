f01d37433b9af987ca8fc915c72019856d0b498c
Jefer Setiawan <jefersetiawan04@gmail.com>
2022-06-30 21:38:42 +0700

    iPense

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,23 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, useColorMode, Button } from "@chakra-ui/react";
 import React from "react";
 
 export default function Header() {
+    const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <Text
-            bgGradient='linear(to-l, #7928CA, #FF0080)'
-            bgClip='text'
-            fontSize={{ base: '5xl', md: '6xl', lg: '7xl' }}
-            fontWeight='bold'
-        >
-            TracPense
-        </Text>
+        <Box className="Header">
+            {/* <Heading>
+                <Button onClick={toggleColorMode}>
+                    Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+                </Button>
+            </Heading> */}
+            <Text
+                bgGradient='linear(to-l, #7928CA, #FF0080)'
+                bgClip='text'
+                fontSize={{ base: '5xl', md: '6xl', lg: '7xl' }}
+                fontWeight='bold'
+            >
+                iPense
+            </Text>
+        </Box>
     )
 }
